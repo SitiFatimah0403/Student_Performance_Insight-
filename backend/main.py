1ae5e3ba45ce5a5371317eccecc0d4da287d78b9
SitiFatimah0403 <223846@student.upm.edu.my>
2025-08-24 12:58:35 +0800

    fix model logic

--- backend/main.py
+++ backend/main.py
@@ -4,26 +4,28 @@ import joblib
 import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
 
-app= FastAPI()
+app = FastAPI()
 model = joblib.load("student_model.joblib")
 
-class Input(BaseModel) :
-    hours_studied : float
-    sleep_hours : float
+class Input(BaseModel):
+    hours_studied: float
+    sleep_hours: float
 
 @app.post("/predict") 
-def predict(data: Input) : 
-    x= np.array([[data.hours_studied, data.sleep_hours]])
-    score = model.predict(x)[0] 
-    return {"predicted_exam_score": round(float(score), 2)}
+def predict(data: Input):
+    x = np.array([[data.hours_studied, data.sleep_hours]])
+    pred = model.predict(x)[0]          # use 'pred' directly
+    pred = np.clip(pred, 0, 100)        # cap between 0 and 100
+    return {"predicted_exam_score": round(pred, 2)}
 
 @app.get("/pbi-embed-token")
-def get_fake_token(): 
+def get_pbi_embed_info(): 
     return {
-        "token": "dummy-token",
-        "embedUrl": "https://app.powerbi.com/view?r=your_fake_report_id",
-        "reportId": "fake-report-id"
+        "embedUrl": "https://app.powerbi.com/reportEmbed?reportId=57d37989-5615-4ee4-b224-d12770e334b3&autoAuth=true&ctid=1f551aeb-7ea1-472c-9ac0-09de9bf33051&actionBarEnabled=true",
+        "reportId": "57d37989-5615-4ee4-b224-d12770e334b3",
+        "token": None  # None for public demo
     }
+
 
 origins = [
     "http://localhost:5173",  # frontend dev server
